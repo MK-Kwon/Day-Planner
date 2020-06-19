52361202d4c4032b0bb19fbd7792eb5f1274b75e
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-06-20 01:10:54 +0930

    Added a function for 'Set interval'

--- Assets/javascript/save.js
+++ Assets/javascript/save.js
@@ -1,38 +1,55 @@
-$(document).ready(function() {
+$(document).ready(function () {
     let currentDate = moment().format("dddd, MMMM Do YYYY");
     let currentTime = moment().format("hh:mm:ss A");
-    let currentHour ;
+    let currentHour;
     const possibleHours = {
-       before: [
-           "12AM",
-           "01AM",
-           "02AM",
-           "03AM",
-           "04AM",
-           "05AM",
-           "06AM"
-       ],
-       work: [
-           "07AM",
-           "08AM",
-           "09AM",
-           "10AM",
-           "11AM",
-           "12PM",
-           "01PM",
-           "02PM",
-           "03PM",
-           "04PM",
-           "05PM"
-       ],
-       after: [
-           "06PM",
-           "07PM",
-           "08PM",
-           "09PM",
-           "10PM",
-           "11PM"
-       ]
+        before: [
+            "12AM",
+            "01AM",
+            "02AM",
+            "03AM",
+            "04AM",
+            "05AM",
+            "06AM"
+        ],
+        work: [
+            "07AM",
+            "08AM",
+            "09AM",
+            "10AM",
+            "11AM",
+            "12PM",
+            "01PM",
+            "02PM",
+            "03PM",
+            "04PM",
+            "05PM"
+        ],
+        after: [
+            "06PM",
+            "07PM",
+            "08PM",
+            "09PM",
+            "10PM",
+            "11PM"
+        ]
     };
+
+    function init() {
+        $("#date").text(currentDate);
+        $("#time").text(currentTime);
+
+        timeColor();
+
+        let currentTimer = setInterval(function () {
+            currentDate = moment().format("dddd, MMMM Do YYYY");
+            $("#date").text(currentDate);
+            currentTime = moment().format("hh:mm:ss A");
+            $("#time").text(currentTime);
+        }, 1000);
+    }
+
     
+
+    init();
 });
