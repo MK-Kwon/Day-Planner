b65bbf70c176796256ec08a04a6b5000a693cf1a
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-06-20 16:28:26 +0930

    Added a function to store 'memo' text in local storage in 'save.js' file

---
+++ Assets/javascript/save.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+    $(".saveBtn").on("click", function(){
+        let clickId = $(this).attr("id");
+        let textId;
+        switch(clickId) {
+            case "07AMsave":
+                textId = $("#07AM").val();
+                localStorage.setItem("07AMtext", textId);
+            break;
+        }
+        switch(clickId) {
+            case "08AMsave":
+                textId = $("#08AM").val();
+                localStorage.setItem("08AMtext", textId);
+            break;
+        }
+        switch(clickId) {
+            case "09AMsave":
+                textId = $("#09AM").val();
+                localStorage.setItem("09AMtext", textId);
+            break;
+        }
+        switch(clickId) {
+            case "10AMsave":
+                textId = $("#10AM").val();
+                localStorage.setItem("10AMtext", textId);
+            break;
+        }
+        switch(clickId) {
+            case "11AMsave":
+                textId = $("#11AM").val();
+                localStorage.setItem("11AMtext", textId);
+            break;
+        }
+        switch(clickId) {
+            case "12PMsave":
+                textId = $("#12PM").val();
+                localStorage.setItem("12PMtext", textId);
+            break;
+        }
+        switch(clickId) {
+            case "01PMsave":
+                textId = $("#01PM").val();
+                localStorage.setItem("01PMtext", textId);
+            break;
+        }
+        switch(clickId) {
+            case "02PMsave":
+                textId = $("#02PM").val();
+                localStorage.setItem("02PMtext", textId);
+            break;
+        }
+        switch(clickId) {
+            case "03PMsave":
+                textId = $("#03PM").val();
+                localStorage.setItem("03PMtext", textId);
+            break;
+        }
+        switch(clickId) {
+            case "04PMsave":
+                textId = $("#04PM").val();
+                localStorage.setItem("04PMtext", textId);
+            break;
+        }
+        switch(clickId) {
+            case "05PMsave":
+                textId = $("#05PM").val();
+                localStorage.setItem("05PMtext", textId);
+        }
+    });
+});
